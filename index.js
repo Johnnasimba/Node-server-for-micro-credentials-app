768508e61fa0c94a415bbdfee09a592cd16f348c
JOHN NASIMBA <nasimba4john@gmail.com>
2021-03-01 18:10:52 +0200

    Removed connection file

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import routes from './src/routes/Routes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import databaseUrl from './connection.js'
 
 const app = express();
 const PORT = 3000;
 
+// mongoose connections
+mongoose.Promise = global.Promise;
+mongoose.connect(databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+// bodyParser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Routes 
 routes(app);
 
 app.get('/', (req, res) => {
